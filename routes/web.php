cffa82b52d541c0feba41cb190adf1afaadeb0e5
Armand Maulana Andika Putra <andikaputrasamawa@gmail.com>
2024-03-01 13:25:50 +0700

    feat: basic parameter

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,65 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/w', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'world';
+});
+
+Route::get('/', function(){
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function(){
+    return 'NIM : 2241720090';
+});
+
+// Route::get('/user/{name}', function($name){
+//     return 'Nama Saya '.$name;
+// });
+
+Route::get('/posts/{post}/comment/{comment}', function ($postId, $commentId){
+    return 'Post ke-'.$postId." Komentar ke-".$commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function(){
+    //
+}) -> name('profile');
+
+Route::get(
+    '/user/profile',
+    [UserProfileController::class, 'show']
+) -> name('profile');
+
+//Generating Urls..
+$url = route('profile');
+
+//Generating Redirects...
+return redirect() -> route('profile');
+
+// Route::get('/posts/{post}/comments/{comment}', function
+// ($postId, $commentId) {
+//  return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+// });
+
+
+
+
+
+
+
